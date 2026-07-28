eb5b4ccee00809d151bf1d87c9bd7cea009bf633
Bradford-Howe <63121524+Bradford-Howe@users.noreply.github.com>
2026-07-27 19:11:58 -0500

    Lab 1 quarto building

--- Lab1.R
+++ Lab1.R
@@ -1,8 +1,8 @@
-DataForPlot = mtcars
 
 
 
 ##############################################
+DataForPlot = mtcars
 
 #Set a serifed font
 par(family = "serif")
@@ -33,7 +33,7 @@ title(main = expression(italic("Frequency Distribution of Carburetors in the mtc
       line = 1.8)
 
 # === LEFT-JUSTIFIED NOTE (best method for APA) ===
-mtext(expression(paste(italic("Note."), " This is where the note goes. Additional text can continue here as needed.")),
+mtext(expression(paste(italic("Note."), " This is where the note goes.")),
       side = 1,          # bottom
       line = 3.8,        # distance below x-axis (adjust as needed)
       adj = 0,           # 0 = left justified
@@ -41,3 +41,56 @@ mtext(expression(paste(italic("Note."), " This is where the note goes. Additiona
 
 #Restore default font
 par(family = "")
+
+
+##############################################
+##############################################
+
+# Create frequency table for carburetors 0–9, even though several of those numbers
+# have zero fequency; i.e., there are no cars with 0 or 5 carburetors. This
+# leaves all options open for making a frequency table.
+carb = factor(mtcars$carb, levels = 0:9)
+
+# This set of functions gives us the frequency for each level we just made and
+# makes the table have the right format for our work.
+freq = as.numeric(table(carb))
+
+# This gives us the total number of engines measured.
+freq_total = sum(freq)
+
+# Relative frequency is the frequency of a certain value relative to the total.
+rel_freq = freq / freq_total
+
+# This function adds in sequence the frequencies we have measured; i.e., the 
+# number of engines with 0 carbs. plus those with 1, plus those with 2...
+cumu_f = cumsum(freq)
+
+# The converts the cumulative fequency to a percentage of the total.
+cumu_pct = cumu_f / n * 100
+
+
+# Build the table. In the data.frame function you write the name of the column,
+# then the equal sign, then what you want to go in that column.
+tab = data.frame(
+   Carburetors = 0:9, # Make a line in the table for each value
+   f = freq,
+   `Relative f` = round(rel_f,2), # Rounds to two decimal places
+   `Cumulative f` = cum_f,
+   `Cumulative %` = round (cumu_pct),
+   check.names = F
+)
+
+# Add Total row
+tab = rbind(tab, data.frame(
+   Carburetors = "Total",
+   f = n,
+   `Relative f` = "",
+   `Cumulative f` = "",
+   `Cumulative %` = "",
+   check.names = FALSE
+))
+
+# Print with APA-style title
+cat("Table 1\n\n")
+cat("Frequencies of the Number of Carburetors in Various Car Models\n\n")
+print(tab, row.names = FALSE)
