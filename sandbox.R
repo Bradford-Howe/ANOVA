f7c750435703930abf8181296c6aa2841ebbfab7
Bradford-Howe <63121524+Bradford-Howe@users.noreply.github.com>
2026-08-11 15:53:30 -0500

    A3 began

--- sandbox.R
+++ sandbox.R
@@ -7,18 +7,21 @@ library(ggpubr)
 library(emmeans)
 library(psych)
 
-for (i in 1:4)
-{
-      a=t.test(iris[iris$Species == levels(iris$Species)[1],i],
-             iris[iris$Species == levels(iris$Species)[2],i])
-      b=t.test(iris[iris$Species == levels(iris$Species)[1],i],
-             iris[iris$Species == levels(iris$Species)[3],i])
-      c=t.test(iris[iris$Species == levels(iris$Species)[2],i],
-             iris[iris$Species == levels(iris$Species)[3],i])
-      print(a)
-      print(b)
-      print(c)
-}
 
-t.test(iris[iris$Species == levels(iris$Species)[2],2],
-       iris[iris$Species == levels(iris$Species)[3],2])
+Q3df = data.frame( Group1 = c(6,4,5,3,6,2,4,5,7,5),
+                   Group2 = c(12,8,9,7,8,11,7,11,19,11)
+                  )
+	
+
+write.table(Q3df, "A2_Q3_data.csv",sep = ",",
+            row.names = F, quote = F)
+
+
+cookies = data.frame(
+   Vanilla = c(1, 3, 1, 3, 1),
+   Chocolate = c(5, 6, 2, 4, 5),
+   PeanutButter = c(7, 3, 5, 5, 6)
+)
+
+write.table(cookies, "A2_Q2_data.csv",sep = ",",
+            row.names = F, quote = F)
